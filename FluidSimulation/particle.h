7dfdd0b75535103dc5cf7358ac5e4cc449cea690
Yifan Wang <phenomenon@yifandemacbook-pro.local>
2018-03-24 18:27:35 -0400

    Setup

---
+++ FluidSimulation/particle.h
@@ -0,0 +1,15 @@
+//
+//  particle.h
+//  FluidSimulation
+//
+//  Created by Yifan Wang on 2018/3/24.
+//  Copyright © 2018年 Yifan Wang. All rights reserved.
+//
+
+#ifndef particle_h
+#define particle_h
+#endif /* particle_h */
+
+struct Particle {
+    <#struct fields#>
+};
